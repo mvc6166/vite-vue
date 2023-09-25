c8cd42f18868432820b49183bd46085d11883e98
Plasmashark 4 <mvc6166@psu.edu>
2023-09-24 21:22:59 -0400

    added card.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,18 @@ defineProps({
 })
 
 const count = ref(0)
+
+document.querySelector('.details').addEventListener('click', (e) => {
+  const crd = document.querySelector('.card p');
+  const disp = crd.style.display;
+  
+  if (disp === 'none') {
+    crd.style.display = 'block';
+  } else {
+    crd.style.display = 'none';
+  }
+});
+
 </script>
 
 <template>
@@ -31,10 +43,72 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
+  <div class="card">
+    <div class="title">
+      <h2>This is the Title of a Card</h2>
+    </div>
+
+    <div class="image">
+      <img src="https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg"
+           alt="user icon"
+           width="400"
+           >
+      <p>Description of the image above. Lorem ipsum dolor sit amet.</p>
+    </div>
+    
+    <button class='details'>
+      <a>Details</a>
+    </button>
+  </div>
+
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
 }
+
+.dupe:hover {
+  background-color: black;
+  color: white;
+}
+
+.dupe:focus {
+  background-color: black;
+  color: white;
+}
+
+.layout {
+  display: flex;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+}
+
+.card {
+  border: 10px solid;
+  margin: auto;
+  width: 500px;
+  padding: 10px;
+  text-align: center;
+  background-color: cyan;
+}
+
+.details {
+    display: inline;
+}
+
+@media screen and (max-width: 800px) {
+  .details {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .card {
+    width = 100%;
+  }
+}
+
 </style>
